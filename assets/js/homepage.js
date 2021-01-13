3a189c0882170a70f5db3bd22639a97f3a9edc50
T. Curry <74472798+curryduz@users.noreply.github.com>
2021-01-13 14:30:00 -0600

    managed to add fetch and locate repo, failed to display response on page

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,4 +1,49 @@
-var getUserRepos = function() {
-  fetch("https://api.github.com/users/octocat/repos");
+var getUserRepos = function(user) {
+  // format the github api url
+  var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+  // make a request to the url
+  fetch(apiUrl).then(function(response) {
+    console.log(response);
+    response.json().then(function(data) {
+      console.log(data);
+    });
+  });
+};
+
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+var formSubmitHandler = function(event) {
+  event.preventDefault();
+  console.log(event);
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
+
+// get value from input element
+var username = nameInputEl.value.trim();
+
+if (username) {
+  getUserRepos(username);
+  nameInputEl.value = "";
+  response.json().then(function(data) {
+  displayRepos(data, user);
+});
+
+} else {
+  alert("Please enter a GitHub username");
+}
+ var repoContainerEl = document.querySelector("#repos-container");
+ var repoSearchTerm = document.querySelector("#repo-search-term");
+
+
+var displayRepos = function(repos, searchTerm) {
+    // clear old content
+  repoContainerEl.textContent = "";
+  repoSearchTerm.textContent = searchTerm;
+  console.log(repos);
+  console.log(searchTerm);
+
 };
 getUserRepos();
